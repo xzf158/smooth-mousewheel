cc781200a75c79a18ec5d7a93039ecc010d6dda9
xuzhanfu <terry@fpi.cc>
2014-02-11 17:24:00 +0800

    update

--- dev/smooth_mousewheel.js
+++ dev/smooth_mousewheel.js
@@ -1,70 +1,66 @@
 /**
  * @author Terry
- * @email terry.xu@me.com
  * @date 2014/2/6
  */
-
- define(["jquery"], function(){
-     var defaultsOption = {
-        spring : .4,
-        duration : 900,
-        maxDetail : 40
+define(["jquery"], function() {
+    var defaultsOption = {
+        spring: .4,
+        duration: 900,
+        maxDetail: 40
     }
-
     var SmoothMousewheel = function(options) {
         this.$e = $(document);
         this.$e[0]._inst = this;
         // this.options = options;
         this._init();
     }, $window = $(window);
-
     SmoothMousewheel.prototype = {
         constructor: SmoothMousewheel,
         options: undefined,
-        targetTop : 0,
-        currentTop : 0,
-        scrollHeight : 0,
-        tmpScroll : null,
-        intervalId : 0,
-        isLock : false,
+        targetTop: 0,
+        currentTop: 0,
+        scrollHeight: 0,
+        tmpScroll: null,
+        intervalId: 0,
+        isLock: false,
         inAnimate: false,
         _init: function() {
             var scope = this;
             this.scrollHeight = document.documentElement.scrollHeight - $window.height();
             scope.targetTop = scope.currentTop = this.$e.scrollTop();
             this.tmpScroll = $("<div/>");
-            $window.on("resize",function (){
+            $window.on("resize", function() {
                 scope.scrollHeight = document.documentElement.scrollHeight - $window.height();
                 scope._forceUpdate();
             });
-            $(document).on("scroll", function (e){
-                if(!scope.inAnimate){
+            $(document).on("scroll", function(e) {
+                if (!scope.inAnimate) {
                     scope._forceUpdate();
                 }
             });
         },
-        _mousewheelEvent: function (e){
+        _mousewheelEvent: function(e) {
             e.preventDefault();
-            if(this._inst.isLock){
+            if (this._inst.isLock) {
                 return;
             }
             var evt = e.originalEvent,
-                detail = evt.detail ? evt.detail * 30 : - evt.wheelDelta / 3;
-
-            if(Math.abs(detail) > this._inst.options.maxDetail){
-                detail = detail > 0 ? this._inst.options.maxDetail : - this._inst.options.maxDetail;
+                detail = evt.detail ? evt.detail * 30 : -evt.wheelDelta / 3;
+            if (Math.abs(detail) > this._inst.options.maxDetail) {
+                detail = detail > 0 ? this._inst.options.maxDetail : -this._inst.options.maxDetail;
             }
             this._inst.targetTop += detail;
-
-            if(this._inst.targetTop < 0){
+            if (this._inst.targetTop < 0) {
                 this._inst.targetTop = 0;
-            }else if(this._inst.targetTop > this._inst.scrollHeight){
+            } else if (this._inst.targetTop > this._inst.scrollHeight) {
                 this._inst.targetTop = this._inst.scrollHeight;
             }
         },
-        _animationLoop: function (){
-            var st = new Date(), et, dt, spring, scope = this;
+        _animationLoop: function() {
+            var st = new Date(),
+                et, dt, spring, scope = this;
             scrollAnimation();
+
             function scrollAnimation() {
                 if (Math.abs(scope.currentTop - scope.targetTop) > 0) {
                     et = new Date();
@@ -93,71 +89,69 @@
                 scope.intervalId = window.requestAnimationFrame(scrollAnimation);
             }
         },
-        _forceUpdate:function (){
+        _forceUpdate: function() {
+            this.scrollHeight = document.documentElement.scrollHeight - $window.height();
             this.currentTop = this.targetTop = this.$e.scrollTop();
             this._triggerEvent();
         },
-        _triggerEvent:function (){
-             $window.trigger($.Event("SmoothScroll",{scrollTop: this.currentTop }));
+        _triggerEvent: function() {
+            $window.trigger($.Event("SmoothScroll", {
+                scrollTop: this.currentTop
+            }));
         },
-        scrollTo: function(top, duration, easing){
+        scrollTo: function(top, duration, easing) {
             var scope = this,
                 lockState = this.isLock;
-
             this.lock();
             this.tmpScroll.css("top", this.currentTop);
             this.tmpScroll.animate({
-                top : top,
+                top: top,
             }, {
-                duration : duration ? duration : scope.options.duration,
-                step : function(now, fx) {
+                duration: duration ? duration : scope.options.duration,
+                step: function(now, fx) {
                     scope.targetTop = parseInt(now);
                 },
-                easing : easing ? easing : "linear",
-                complete : function() {
-                    if(!lockState){
+                easing: easing ? easing : "linear",
+                complete: function() {
+                    if (!lockState) {
                         scope.unlock();
                     }
                 }
             });
         },
-        enable:function (options){
-            if(!this.options || options){
+        enable: function(options) {
+            this._forceUpdate();
+            if (!this.options || options) {
                 this.options = $.extend({}, defaultsOption, typeof options == 'object' && options);
             }
             this.$e.on("mousewheel DOMMouseScroll", this._mousewheelEvent);
             this._animationLoop();
             this._triggerEvent();
         },
-        disable: function (){
+        disable: function() {
             this.$e.off("mousewheel DOMMouseScroll", this._mousewheelEvent);
             window.cancelAnimationFrame(this.intervalId);
         },
-        lock: function (){
-             this.isLock = true;
+        lock: function() {
+            this.isLock = true;
         },
-        unlock: function (){
+        unlock: function() {
+            this._forceUpdate();
             this.isLock = false;
         },
-        distroy: function (){
+        distroy: function() {
             this.disable();
             delete this.$e._inst;
             delete this.$e;
-            for(var i in this){
+            for (var i in this) {
                 delete this[i];
             }
         }
     };
-
-    window.requestAnimFrame = (function () {
-        return  window.requestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.oRequestAnimationFrame ||
-                window.msRequestAnimationFrame ||
-                function (callback) {
-                    window.setTimeout(callback, 1000 / 60);
-                }; 
+    window.requestAnimFrame = (function() {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
     })();
     return new SmoothMousewheel();
- });
+});
